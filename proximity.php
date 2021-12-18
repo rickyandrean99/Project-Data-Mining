d202de6ba2b00f527aea08d8eca916e06ab79f6b
rickyandrean99 <ricky.ralph09@gmail.com>
2021-12-18 20:20:03 +0700

    Update proximity.php

--- proximity.php
+++ proximity.php
@@ -13,9 +13,12 @@
             fclose($open);
         }
         
-        $attribut = []; // index ke 0 jangan dipakai
+        $attribut = [];
         $baris = [];
         $nilai = [];
+        $manhattan_list = [];
+        $euclidean_list = [];
+        $supremum_list = [];
 
         foreach ($data as $key => $value) {
             if ($key == 0) {
@@ -24,6 +27,7 @@
                 foreach ($modify as $value) {
                     array_push($attribut, $value);
                 }
+                array_shift($attribut);
             } else {
                 // Mendapatkan nama baris dan juga datanya dari dataset
                 $modify = explode(";", $value[0]);
@@ -36,98 +40,105 @@
                     array_push($nilai, $value);
                 }
 
-                $nilai_matriks = array_chunk($nilai, count($attribut)-1);
+                $nilai_matriks = array_chunk($nilai, count($attribut));
             }
         }
         
-        // Start: readable data
-        $attribut_list = '';
-        $baris_list = '';
-        $nilai_list = '';
-
-        foreach ($attribut as $key => $value) {
-            if ($key == 0) continue;
-            $attribut_list .= $value;
-            if ($key != count($attribut) - 1) $attribut_list .= ", ";
-        }
-
-        foreach ($baris as $key => $value) {
-            $baris_list .= $value;
-            if ($key != count($baris) - 1) $baris_list .= ", ";
-        }
-        
-        foreach ($nilai_matriks as $key => $value) {
-            foreach ($value as $key2 => $value2) {
-                $nilai_list .= $value2;
-                if ($key2 != count($value) - 1) $nilai_list .= ", ";
-            }
-            $nilai_list .= "<br>";
-        }
-
-        echo "<div style='font-size: 16px'>";
-            echo "<p><b>Attribut list:</b><br>".$attribut_list."</p>";
-            echo "<p><b>Baris list:</b><br>".$baris_list."</p>";
-            echo "<p><b>Nilai list:</b><br>".$nilai_list."</p>";
-        echo "</div>";
-        // End: readable data
-
-        echo "<div style='display: flex; flex-direction: row;'>";
-        
-        echo "<div style='width: 33%'>";
         // Manhattan Distance
         foreach ($nilai_matriks as $key1 => $value1) {
+            $manhattan_row = [];
             foreach ($nilai_matriks as $key2 => $value2) {
-                if ($key1 == $key2) continue;
-
                 $manhattan = 0;
                 foreach ($value2 as $key3 => $value3) {
                     $manhattan += abs($value1[$key3] - $value3);
                 }
-
-                echo "Manhattan(".$baris[$key1].", ".$baris[$key2].") = ".$manhattan."<br>";
+                array_push($manhattan_row, $manhattan); 
             }
-            echo "<br>";
+            array_push($manhattan_list, $manhattan_row);
         }
-        echo "</div>";
 
-        echo "<div style='width: 33%'>";
         // Euclidean Distance
         foreach ($nilai_matriks as $key1 => $value1) {
+            $euclidean_row = [];
             foreach ($nilai_matriks as $key2 => $value2) {
-                if ($key1 == $key2) continue;
-
                 $euclidean = 0;
                 foreach ($value2 as $key3 => $value3) {
                     $euclidean += pow(($value1[$key3] - $value3), 2);
                 }
-
-                echo "Euclidean(".$baris[$key1].", ".$baris[$key2].") = ".round(sqrt($euclidean), 2)."<br>";
+                array_push($euclidean_row, round(sqrt($euclidean), 2));
             }
-            echo "<br>";
+            array_push($euclidean_list, $euclidean_row); 
         }
-        echo "</div>";
 
-        echo "<div style='width: 33%'>";
         // Supremum Distance
         foreach ($nilai_matriks as $key1 => $value1) {
+            $supremum_row = [];
             foreach ($nilai_matriks as $key2 => $value2) {
-                if ($key1 == $key2) continue;
-
                 $supremum = 0;
                 foreach ($value2 as $key3 => $value3) {
                     if (abs($value1[$key3] - $value3) > $supremum) {
                         $supremum = abs(($value1[$key3] - $value3));
                     }
                 }
-
-                echo "Supremum(".$baris[$key1].", ".$baris[$key2].") = ".$supremum."<br>";
+                array_push($supremum_row, $supremum);
             }
-            echo "<br>";
+            array_push($supremum_list, $supremum_row);
         }
-        echo "</div>";
 
-        echo "</div>";
+        $distances = [];
+        $distances["Manhattan"] = $manhattan_list;
+        $distances["Euclidean"] = $euclidean_list;
+        $distances["Supremum"] = $supremum_list;
     } else {
         header("location: index.php");
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Project Data Mining</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="d-flex flex-column justify-content-center" style="height: 80vh">
+            <div class="h2 text-center my-4 fw-bold">Proximity Matrix</div>
+            <div class="d-flex flex-row flex-wrap mt-4 justify-content-center">
+                <?php foreach ($distances as $index => $val) { ?>
+                    <div class="mx-3">
+                        <table style='border: 1px solid black; border-collapse: collapse'>
+                            <thead>
+                                <tr>
+                                    <th colspan="<?php echo (count($baris)+1); ?>" style='padding: 5px 25px; border: 1px solid black; text-align: center'><?php echo $index; ?> Distance</th>
+                                </tr>
+                                <tr>
+                                    <th style='padding: 5px 25px; border: 1px solid black;'></th>
+                                    <?php foreach ($baris as $key => $value) { ?>
+                                        <th style='padding: 5px 25px; border: 1px solid black;' class="text-center"><?php echo $value; ?></th>
+                                    <?php } ?>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php foreach ($val as $key => $value) { ?>
+                                    <tr>
+                                        <td style='padding: 5px 25px; border: 1px solid black;' class="text-center"><?php echo $baris[$key]; ?></td>
+                                        <?php foreach ($value as $key2 => $value2) { ?>
+                                            <td style='padding: 5px 25px; border: 1px solid black;' class="text-center"><?php echo $value2; ?></td>
+                                        <?php } ?>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </body>
+</html>
